1d6d12b73fa32ee8f0b0cd80b107606bb61cc2be
Petr Janik <485122@mail.muni.cz>
2020-10-27 12:13:38 +0100

    Add react-native-appearance. App is crashing on start because of: Error: Unable to resolve module `./debugger-ui/debuggerWorker.cff11639` from ``: ./debugger-ui/debuggerWorker.cff11639 could not be found within the project.

--- src/components/DarkModeDemo.tsx
+++ src/components/DarkModeDemo.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Typography from '../global/styles/typography';
+import {useColorScheme} from 'react-native-appearance';
 
 const DarkModeDemo = () => {
+  const colorScheme = useColorScheme();
+
   return (
     <View>
       <Text style={styles.header}>Some primary color text</Text>
       <Text style={styles.subheader}>Some secondary color text</Text>
-      <Text style={styles.content}>Some text that ignores theme</Text>
+      <Text
+        style={
+          styles.content
+        }>{`Some text that ignores theme. The system color scheme is ${colorScheme}`}</Text>
     </View>
   );
 };
