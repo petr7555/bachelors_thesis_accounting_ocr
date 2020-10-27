ba1841557f0513f6ac226b6336a5ba1eda313a99
Petr Janik <485122@mail.muni.cz>
2020-10-28 00:52:16 +0100

    Add switch (toggle) to SettingsScreen.tsx

--- src/components/SettingsScreen.tsx
+++ src/components/SettingsScreen.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, Switch, StyleSheet} from 'react-native';
 
 const SettingsScreen = () => {
-  return <Text>This is Settings screen</Text>;
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
+  return (
+    <View style={styles.container}>
+      <Text>This is Settings screen</Text>
+      <Switch
+        trackColor={{false: '#767577', true: '#81b0ff'}}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
 export default SettingsScreen;
