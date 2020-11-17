be3934c8e77082e78fac549afd6dc7b25d961717
Petr Janik <485122@mail.muni.cz>
2020-11-17 11:55:06 +0100

    🚀 Improve ReceiptsList.tsx. Use FlatList instead of mapping over receipts.

--- src/components/ReceiptsList.tsx
+++ src/components/ReceiptsList.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import {FirebaseFirestoreTypes} from '@react-native-firebase/firestore';
-import {ActivityIndicator, View} from 'react-native';
+import {ActivityIndicator, Alert, FlatList, ListRenderItem} from 'react-native';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {Avatar, ListItem} from 'react-native-elements';
 import {authInstance, firestoreInstance} from '../global/firebase';
 import {useCollectionData} from 'react-firebase-hooks/firestore';
 
-type Receipt = DocumentReceipt & {id: string};
-
-type DocumentReceipt = {
+type Receipt = {
+  id: string;
   url: string;
   added: FirebaseFirestoreTypes.Timestamp;
 };
 
 const ReceiptsList = () => {
   const [user, loadingUser, errorUser] = useAuthState(authInstance);
-  const [
-    receipts = [],
-    loadingReceipts,
-    errorReceipts,
-  ] = useCollectionData(
+  const [receipts = [], loadingReceipts, errorReceipts] = useCollectionData<
+    Receipt
+  >(
     firestoreInstance.collection('Users').doc(user?.uid).collection('receipts'),
     {idField: 'id'},
   );
@@ -28,21 +25,36 @@ const ReceiptsList = () => {
     return <ActivityIndicator />;
   }
 
+  if (errorUser) {
+    console.log(errorUser);
+    Alert.alert('Cannot load current user.');
+  }
+
+  if (errorReceipts) {
+    console.log(errorReceipts);
+    Alert.alert('Cannot load receipts.');
+  }
+
+  const renderItem: ListRenderItem<Receipt> = ({item}) => {
+    return (
+      <ListItem bottomDivider>
+        <Avatar source={{uri: item.url}} />
+        <ListItem.Content>
+          <ListItem.Title>{item.url}</ListItem.Title>
+          <ListItem.Subtitle>
+            {item.added.toDate().toDateString()}
+          </ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  };
+
   return (
-    <View>
-      {receipts.map((receipt) => (
-        <ListItem key={receipt.id} bottomDivider>
-          <Avatar source={{uri: receipt.url}} />
-          <ListItem.Content>
-            <ListItem.Title>{receipt.url}</ListItem.Title>
-            <ListItem.Subtitle>
-              {receipt.added.toDate().toDateString()}
-            </ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
-      ))}
-    </View>
+    <FlatList
+      keyExtractor={(item) => item.id}
+      data={receipts}
+      renderItem={renderItem}
+    />
   );
 };
-
 export default ReceiptsList;
