e8cfd17a071404102059d31343da37e850d0aecc
Petr Janik <peta.janik@email.cz>
2021-03-06 21:53:13 +0100

    🔨 Stories with new syntax are not supported on React Native

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,29 +1,18 @@
 import { action } from '@storybook/addon-actions';
 import { text } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 import { Text } from 'react-native';
 import Button from './Button';
 
-export default {
-  title: 'Button',
-};
-
-export const WithText = () => (
-  <Button onPress={action('clicked-text')}>
-    <Text>{text('Button text', 'Hello Button')}</Text>
-  </Button>
-);
-
-WithText.story = {
-  name: 'with text',
-};
-
-export const WithSomeEmoji = () => (
-  <Button onPress={action('clicked-emoji')}>
-    <Text>😀 😎 👍 💯</Text>
-  </Button>
-);
-
-WithSomeEmoji.story = {
-  name: 'with some emoji',
-};
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onPress={action('clicked-text')}>
+      <Text>{text('Button text', 'Hello Button')}</Text>
+    </Button>
+  ))
+  .add('with some emoji', () => (
+    <Button onPress={action('clicked-emoji')}>
+      <Text>😀 😎 👍 💯</Text>
+    </Button>
+  ));
