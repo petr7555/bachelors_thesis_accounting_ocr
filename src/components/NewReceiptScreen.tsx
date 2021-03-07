be5e878caa898571a63bc78fe37134e2933b628e
Petr Janik <peta.janik@email.cz>
2021-03-07 20:40:45 +0100

    🎨 Extract styles

--- src/components/NewReceiptScreen.tsx
+++ src/components/NewReceiptScreen.tsx
@@ -1,17 +1,21 @@
-import { useTheme } from '@react-navigation/native';
-import { Text, View } from 'react-native';
 import * as React from 'react';
+import { useTheme } from '@react-navigation/native';
+import { StyleSheet, Text, View } from 'react-native';
 import Form from './Form';
 
 const NewReceiptScreen = () => {
   const { colors } = useTheme();
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={styles.container}>
       <Text style={{ color: colors.text }}>New receipt!</Text>
       <Form />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+});
 
 export default NewReceiptScreen;
