006e3cf17eb3da7ff9480af13ecb748c96173dde
Petr Janik <peta.janik@email.cz>
2021-03-07 23:08:56 +0100

    🔨 Fix path to storybook-rn

--- tests/snapshot/storyshots.test.ts
+++ tests/snapshot/storyshots.test.ts
@@ -2,7 +2,7 @@ import initStoryshots from '@storybook/addon-storyshots';
 
 initStoryshots({
   // from project root
-  configPath: './storybook-rn',
+  configPath: './.storybook-rn',
   // optional
   framework: 'react-native',
 });
