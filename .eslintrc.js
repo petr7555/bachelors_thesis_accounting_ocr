05c99b2c09e2317fdc5a7f5dc3a840c178fb9bc2
Petr Janik <peta.janik@email.cz>
2021-03-07 20:51:15 +0100

    🔨 ESLint does not complain about non existing global test methods

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,15 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  globals: {
+    beforeAll: false,
+    afterAll: false,
+    beforeEach: false,
+    afterEach: false,
+    test: false,
+    expect: false,
+    describe: false,
+    jest: false,
+    it: false,
+  },
 };
