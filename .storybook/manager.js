055d503b717c06c9058bf96c872fa6f8677da281
Petr Janik <peta.janik@email.cz>
2021-03-09 19:53:05 +0100

    🎨 Fix formatting

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,13 +2,13 @@ import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming/create';
 
 const theme = create({
-    base: 'light',
-    brandTitle: 'Receipts Scanner',
-    brandUrl: 'https://github.com/petr7555/bachelors_thesis_accounting_ocr',
-    brandImage:
-        'https://storage.googleapis.com/images_bachelorsthesisaccountingocr/receipt-icon.png',
+  base: 'light',
+  brandTitle: 'Receipts Scanner',
+  brandUrl: 'https://github.com/petr7555/bachelors_thesis_accounting_ocr',
+  brandImage:
+    'https://storage.googleapis.com/images_bachelorsthesisaccountingocr/receipt-icon.png',
 });
 
 addons.setConfig({
-    theme,
+  theme,
 });
