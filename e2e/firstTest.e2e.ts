5f4000ecece14c2dbfde65b220c90e2b507e8718
Petr Janik <peta.janik@email.cz>
2021-03-13 11:01:59 +0100

    🎨 Refactor e2e tests initialization

--- e2e/firstTest.e2e.ts
+++ e2e/firstTest.e2e.ts
@@ -1,10 +1,6 @@
 import { by, device, element, expect } from 'detox';
 
 describe('Example', () => {
-  beforeAll(async () => {
-    await device.launchApp();
-  });
-
   beforeEach(async () => {
     await device.reloadReactNative();
   });
