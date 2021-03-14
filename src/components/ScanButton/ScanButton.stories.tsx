2e7866021b4e36bd3e101045c799e88b47691473
Petr Janik <peta.janik@email.cz>
2021-03-15 00:27:18 +0100

    🔨 Fix tests by wrapping ScanButton component into MockedNavigator

--- src/components/ScanButton/ScanButton.stories.tsx
+++ src/components/ScanButton/ScanButton.stories.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import ScanButton from './ScanButton';
+import MockedNavigator from '../../../tests/mocks/MockedNavigator';
 
-storiesOf('ScanButton', module).add('default', () => <ScanButton />);
+storiesOf('ScanButton', module).add('default', () => (
+  <MockedNavigator component={ScanButton} />
+));
---
+++ tests/mocks/MockedNavigator.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const MockedNavigator = ({
+  component,
+  params = {},
+}: {
+  component: any;
+  params?: any;
+}) => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="MockedScreen"
+          component={component}
+          initialParams={params}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MockedNavigator;
