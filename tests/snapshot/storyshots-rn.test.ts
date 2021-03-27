956ab03fcfabbc261f3b958821dfa8897980a2fd
Petr Janik <peta.janik@email.cz>
2021-03-27 21:49:45 +0100

    🚀 Run both RN Web and RN storyshots

---
+++ tests/snapshot/storyshots-rn.test.ts
@@ -0,0 +1,7 @@
+import initStoryshots from '@storybook/addon-storyshots';
+
+initStoryshots({
+  // from project root
+  configPath: './.storybook-rn',
+  framework: 'react-native',
+});
--- tests/snapshot/storyshots-web.test.ts
+++ tests/snapshot/storyshots-web.test.ts
@@ -2,7 +2,5 @@ import initStoryshots from '@storybook/addon-storyshots';
 
 initStoryshots({
   // from project root
-  configPath: './.storybook-rn',
-  // optional
-  framework: 'react-native',
+  configPath: './.storybook',
 });
