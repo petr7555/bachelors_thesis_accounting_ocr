3178ff63ec4043065de9e44bb52345e3831d287f
Petr Janik <peta.janik@email.cz>
2021-04-04 11:42:46 +0200

    🚀 Add RGB equivalents to common colors

--- src/global/styles/colors.ts
+++ src/global/styles/colors.ts
@@ -1,8 +1,8 @@
 const Colors = {
-  primary: '#078331',
-  secondary: '#310783',
+  primary: '#078331', // rgb(7,131,49)
+  secondary: '#310783', // rgb(49,7,131)
   // unused so far, should fit nicely with primary and secondary
-  tertiary: '#833107',
+  tertiary: '#833107', // rgb(131,49,7)
   red: '#e00000',
   green: 'green',
   transparentBlack: 'rgba(0, 0, 0, 0.5)',
