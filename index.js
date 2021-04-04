4e921c13e57694b486a0c0c3ce59ed2dc8705c4d
Petr Janik <peta.janik@email.cz>
2021-04-04 11:44:16 +0200

    🔨 Storybook RN folder must not be hidden (i.e. start with a dot), otherwise Metro bundler cannot import it as a module.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { name as appName } from './app.json';
 LogBox.ignoreLogs(['AsyncStorage has been extracted']);
 // Emitted by running Loki visual tests
 LogBox.ignoreLogs(['console.disableYellowBox']);
-//Ignore all log notifications
+// Ignore all log notifications
 // LogBox.ignoreAllLogs();
 
 // Should we show storybook instead of our app?
@@ -17,7 +17,7 @@ const VISUAL_TESTS = false;
 let RootComponent = App;
 // Only include Storybook if we're in dev mode
 if (__DEV__ && SWITCH_STORYBOOK_FROM_REACTOTRON) {
-  const { StorybookUIRoot } = require('./.storybook-rn');
+  const { StorybookUIRoot } = require('./storybook-rn');
   const Reactotron = require('./src/services/reactotron');
   const reactotron = new Reactotron.Reactotron();
   reactotron.setup();
@@ -25,7 +25,7 @@ if (__DEV__ && SWITCH_STORYBOOK_FROM_REACTOTRON) {
 }
 
 if (__DEV__ && VISUAL_TESTS) {
-  const { StorybookUIRoot } = require('./.storybook-rn');
+  const { StorybookUIRoot } = require('./storybook-rn');
   RootComponent = StorybookUIRoot;
 }
 
