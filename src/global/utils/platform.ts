a936a6a05417c1b995a3020a37e33ec8acaa26b7
Petr Janik <peta.janik@email.cz>
2021-04-22 12:22:19 +0200

    🚀 Make FullWidthImage.tsx work also in Storybook for Web. The downside is that the height is computed from the whole screen width, not just the parent container width, therefore when it is e.g. in modal, this solution won't be optimal.

--- src/global/utils/platform.ts
+++ src/global/utils/platform.ts
@@ -3,3 +3,5 @@ import { Platform } from 'react-native';
 export const isAndroid = Platform.OS === 'android';
 
 export const isWindows = Platform.OS === 'windows';
+
+export const isWeb = Platform.OS === 'web';
