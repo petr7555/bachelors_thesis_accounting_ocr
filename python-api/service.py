062c918e7a2041daa7f8d0d9ddda97b0361cdd16
Petr Janik <peta.janik@email.cz>
2021-04-26 11:39:41 +0200

    Revert "🚧 Keep only hello world in flask app"
    This reverts commit e118bcaa973aa8adfe6ffa414f89bf5176130199.

--- python-api/service.py
+++ python-api/service.py
@@ -7,8 +7,8 @@ import numpy as np
 from PIL import Image
 from flask import Flask, request
 
-# from image_model import process_image
-# from nlp_model import category
+from image_model import process_image
+from nlp_model import category
 
 app = Flask(__name__)
 
@@ -18,30 +18,30 @@ def welcome():
     return "Welcome to the Machine Learning REST API for Receipts Scanner!"
 
 
-# @app.route("/category", methods=['POST'])
-# def category_route():
-#     request_data = request.get_json()
-#     return category(request_data["sentence"])
-# 
-# 
-# @app.route("/process-image", methods=['POST'])
-# def process_image_route():
-#     # get image from form data as base64, read to bytes
-#     file = request.files['image'].read()
-#     img_arr = np.frombuffer(file, np.uint8)
-#     img = cv2.imdecode(img_arr, cv2.IMREAD_GRAYSCALE)
-#     # do the processing
-#     img = process_image(img)
-#     # end of processing
-#     img = Image.fromarray(img.astype("uint8"))
-#     rawBytes = io.BytesIO()
-#     img.save(rawBytes, "JPEG")
-#     rawBytes.seek(0)
-#     img_base64 = base64.b64encode(rawBytes.read())
-#     return {
-#         "image": str(img_base64).split("'")[1],  # removes 'b from the beginning
-#         "mime": "image/jpeg"
-#     }
+@app.route("/category", methods=['POST'])
+def category_route():
+    request_data = request.get_json()
+    return category(request_data["sentence"])
+
+
+@app.route("/process-image", methods=['POST'])
+def process_image_route():
+    # get image from form data as base64, read to bytes
+    file = request.files['image'].read()
+    img_arr = np.frombuffer(file, np.uint8)
+    img = cv2.imdecode(img_arr, cv2.IMREAD_GRAYSCALE)
+    # do the processing
+    img = process_image(img)
+    # end of processing
+    img = Image.fromarray(img.astype("uint8"))
+    rawBytes = io.BytesIO()
+    img.save(rawBytes, "JPEG")
+    rawBytes.seek(0)
+    img_base64 = base64.b64encode(rawBytes.read())
+    return {
+        "image": str(img_base64).split("'")[1],  # removes 'b from the beginning
+        "mime": "image/jpeg"
+    }
 
 
 if __name__ == "__main__":
