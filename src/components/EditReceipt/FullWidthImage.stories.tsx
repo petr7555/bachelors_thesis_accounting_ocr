df2a11cceb7377104bc10ebf125fb9dad79891c4
Petr Janik <peta.janik@email.cz>
2021-05-01 23:32:52 +0200

    🎨 Exchange receipt in storybook for my own

--- src/components/EditReceipt/FullWidthImage.stories.tsx
+++ src/components/EditReceipt/FullWidthImage.stories.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 import FullWidthImage from './FullWidthImage';
 
 export const Basic = () => (
-  // TODO add english receipt form Verca
-  <FullWidthImage uri="https://media-cdn.tripadvisor.com/media/photo-s/0b/91/f0/0f/receipt.jpg" />
+  <FullWidthImage uri="https://storage.googleapis.com/images_bachelorsthesisaccountingocr/sample_receipt_01.jpg" />
 );
 
 storiesOf('FullWidthImage', module).add('Basic', Basic);
