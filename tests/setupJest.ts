d4e3441823020471ca2cf2bc5d32aabe0470e629
Petr Janik <peta.janik@email.cz>
2021-05-08 20:28:46 +0200

    ✅ Mock react-native-fs in tests

--- tests/setupJest.ts
+++ tests/setupJest.ts
@@ -4,6 +4,7 @@ import './mocks/mock-native-animated-helper';
 import './mocks/mock-native-event-emitter';
 import './mocks/mock-react-native-permissions';
 import './mocks/mock-react-native-netinfo';
+import './mocks/mock-react-native-fs';
 
 // this mock would be used if we accessed #react-native-firebase from tests,
 // but since we access web firebase, it is not needed
