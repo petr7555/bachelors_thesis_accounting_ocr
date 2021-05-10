65462223f699c76335a5265e7bc00a71d0270956
Petr Janik <peta.janik@email.cz>
2021-05-10 19:47:29 +0200

    🔨 Fix NoReceipts.stories.tsx to use the right component

--- src/components/ReceiptsList/NoReceipts.stories.tsx
+++ src/components/ReceiptsList/NoReceipts.stories.tsx
@@ -1,6 +1,6 @@
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
-import NoReceipts from './CartImage';
+import NoReceipts from './NoReceipts';
 
 export const Basic = () => <NoReceipts />;
 
